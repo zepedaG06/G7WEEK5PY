e62c43605a78aa3948ae969743046fd852dbb5aa
zepedaG06 <jazepeda@uamv.edu.ni>
2025-04-23 14:00:58 -0600

    Numero par y positivo

---
+++ Desktop/VSTUDIOCODE/Numero_par_positivo.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingrese un numero: "))
+if(numero > 0 and numero %2 == 0):
+    print("El numero es positivo y par")
+else:
+    print("no cumple con los parametros")
+    
+
+    
